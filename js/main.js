82f530d243853631092e1855e7ed188ad3b337f9
Ryan Ho <ryan@horyan.com>
2020-05-07 02:01:47 -0400

    Toggle disabled attr, add status subheading and stop-visual

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 // TODOs:
-// start remaps to stop functionality
-// real-time status subheading: Pending / Waiting for input..., FOCUS MODE ON, BREAK MODE ON
-  // countdown timer
-// disableInputs: grey text, pointer-events: none;
+// add stop functionality
+// include countdown timers
 // roadmap:
   // support decimal minutes 
   // slider toggle: opt in for audio
@@ -17,32 +15,39 @@ function revertDefault(e){
 function startFocus(bgClasses, focusTimer, breakTimer) {
   bgClasses.toggle('blue-bg');
   document.title = 'Hyperfocus | FOCUS';
-  focusTimer.setAttribute('readonly', '');
-  breakTimer.setAttribute('readonly', '');
+  focusTimer.setAttribute('disabled', '');
+  breakTimer.setAttribute('disabled', '');
 }
 
 function configureTimers(bgClasses, focusTimer, breakTimer) {
   const focusMinutes = focusTimer.value * 1000 * 60;
   const breakMinutes = breakTimer.value * 1000 * 60;
-  /*// real-time action button status
-  const actionBtn = document.getElementById('action-btn').textContent = 'Stop';
-  actionBtn.setAttribute('type', 'reset');
-  actionBtn.addEventListener('reset', revertDefault);*/
+  // real-time action button status
+  const actionBtn = document.getElementById('action-btn');
+  actionBtn.textContent = 'Stop';
+  // TODO: add functionality
+  actionBtn.setAttribute('disabled', ''); // temporary
+  //actionBtn.setAttribute('type', 'reset');
+  //actionBtn.addEventListener('reset', revertDefault);*/
   
   setTimeout(() => {
-    focusMinutes === 60000 ? alert('1 minute of focus has passed') : alert(`${focusMinutes/60000} minutes of focus have passed`);
+    focusMinutes === 60000 ? console.log(('1 minute of focus has passed')) : console.log((`${focusMinutes/60000} minutes of focus have passed`));
     
     // start break mode
+    document.getElementById('status').textContent = 'BREAK MODE IN PROGRESS...';
     bgClasses.toggle('blue-bg');
     bgClasses.toggle('green-bg');
     document.title = 'Hyperfocus | BREAK';
     setTimeout(() => {
-      breakMinutes === 60000 ? alert('1 minute of break has passed') : alert(`${breakMinutes/60000} minutes of break have passed`);
+      breakMinutes === 60000 ? console.log(('1 minute of break has passed')) : console.log((`${breakMinutes/60000} minutes of break have passed`));
       // reusable revert default chunk
+      const actionBtn = document.getElementById('action-btn').textContent = 'Start';
+      // TODO: revert start functionality after implementing stop
+      document.getElementById('status').textContent = 'Waiting for input...';
       bgClasses.toggle('green-bg');
       document.title = 'Hyperfocus';
-      focusTimer.removeAttribute('readonly');
-      breakTimer.removeAttribute('readonly');
+      focusTimer.removeAttribute('disabled');
+      breakTimer.removeAttribute('disabled');
     }, breakMinutes);
   }, focusMinutes);
 }
@@ -50,6 +55,10 @@ function configureTimers(bgClasses, focusTimer, breakTimer) {
 
 document.getElementById('interval-form').addEventListener('submit', (e)=>{
   e.preventDefault();
+
+  // status: FOCUS MODE IN PROGRESS
+  document.getElementById('status').textContent = 'FOCUS MODE IN PROGRESS...';
+
   const backgroundClasses = document.getElementById('background').classList;
   const focusInput = document.getElementById('focus-input');
   const breakInput = document.getElementById('break-input');
